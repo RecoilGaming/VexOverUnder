07a009bb37e38606f1dc7ec75c3ae56d269c1cd3
RecoilGaming <36344908+RecoilGaming@users.noreply.github.com>
2023-09-11 20:02:33 -0700

    catapult fixes

--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,8 @@ using namespace vex;
 // function definitions
 void runIntake(void);
 void fireCatapult(void);
+void halfCatapult(void);
+void resetAll(void);
 
 void auton1(void);
 void auton2(void);
@@ -38,7 +40,7 @@ void reset() {
   while (pos >= 360) pos -= 360;
   while (pos <= -360) pos += 360;
 
-  Catapult.setPosition(pos, degrees);
+  if (Catapult.position(degrees) != pos) Catapult.setPosition(pos, degrees);
 }
 
 // setup robot
@@ -69,14 +71,14 @@ void autonomous(void) {
 
   // catapult down
   Catapult.spinToPosition(0, degrees);
-  Catapult.spinToPosition(-354, degrees);
+  Catapult.spinToPosition(-350, degrees);
   
   // drivetrain settings
   Drivetrain.setDriveVelocity(175, rpm);
   Drivetrain.setStopping(hold);
 
   // auton function
-  auton1();
+  // auton1();
 }
 
 // driver controlled function
@@ -86,6 +88,10 @@ void usercontrol(void) {
   Drivetrain.setTurnVelocity(200, rpm);
   Drivetrain.setStopping(coast);
 
+  // catapult down
+  Catapult.spinToPosition(0, degrees);
+  Catapult.spinToPosition(-350, degrees);
+
   while (1) {
     // reset catapult encoders
     reset();
@@ -93,6 +99,8 @@ void usercontrol(void) {
     // robot functions
     runIntake();
     Controller1.ButtonL1.pressed(fireCatapult);
+    Controller1.ButtonL2.pressed(halfCatapult);
+    Controller1.ButtonX.pressed(resetAll);
 
     // waiting
     wait(50, msec);
@@ -144,21 +152,45 @@ void runIntake(void) {
 // void fireCatapult(void) { Catapult.spinFor(reverse, 1, turns); }
 
 void fireCatapult(void) {
-  if (up == true) {
-    Catapult.spinToPosition(-362, degrees);
-    up = false;
-  } else {
-    Catapult.spinToPosition(-354, degrees);
-    up = true;
-  }
+  // if (up == true) {
+  //   Catapult.spinToPosition(-365, degrees);
+  //   up = false;
+  // } else {
+  //   Catapult.spinToPosition(-350, degrees);
+  //   up = true;
+  // }
+
+  Catapult.spinToPosition(-365, degrees);
+  reset();
+  Catapult.spinToPosition(-350, degrees);
+}
+
+void halfCatapult(void) {
+  // if (up == true) {
+  //   Catapult.spinToPosition(-365, degrees);
+  //   up = false;
+  // } else {
+  //   Catapult.spinToPosition(-250, degrees);
+  //   up = true;
+  // }
+
+  Catapult.spinToPosition(-365, degrees);
+  reset();
+  Catapult.spinToPosition(-250, degrees);
+}
+
+void resetAll(void) {
+  Catapult.spinToPosition(0, degrees);
+  PneumaticE.set(false);
+  PneumaticH.set(false);
 }
 
 // auton on same team side
 void auton1(void) {
   // move to goal
-  Drivetrain.driveFor(forward, 24, inches);
+  Drivetrain.driveFor(forward, 23.5, inches);
   Drivetrain.turnFor(left, 70, degrees);
-  Drivetrain.driveFor(forward, 45, inches);
+  Drivetrain.driveFor(forward, 44, inches);
   Drivetrain.turnFor(right, 70, degrees);
 
   // drop triball

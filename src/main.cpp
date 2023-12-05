af7f6c03b4b2730eb9477fae08eb394ed5897a0c
RecoilGaming <qinzhaolee@gmail.com>
2023-12-04 16:13:32 -0800

    New robot code

--- src/main.cpp
+++ src/main.cpp
@@ -12,47 +12,138 @@ const gearSetting blueCartridge = ratio6_1;
 brain robotBrain;
 controller robotController = controller(primary);
 
-motor leftMotorA = motor(PORT2, greenCartridge, false);
-motor leftMotorB = motor(PORT18, greenCartridge, false);
-motor_group leftDrive = motor_group(leftMotorA, leftMotorB);
-motor rightMotorA = motor(PORT1, greenCartridge, true);
-motor rightMotorB = motor(PORT13, greenCartridge, true);
-motor_group rightDrive = motor_group(rightMotorA, rightMotorB);
+motor driveLF = motor(PORT19, greenCartridge, false);
+motor driveLB = motor(PORT20, greenCartridge, false);
+motor_group leftDrive = motor_group(driveLF, driveLB);
+motor driveRF = motor(PORT17, greenCartridge, true);
+motor driveRB = motor(PORT18, greenCartridge, true);
+motor_group rightDrive = motor_group(driveRF, driveRB);
 drivetrain robotDrive = drivetrain(leftDrive, rightDrive, 4*M_PI, 11, 11, inches, 3/7);
 
-motor robotIntake = motor(PORT7, blueCartridge, false);
-motor robotCatapult = motor(PORT3, redCartridge, false);
-rotation catapultSensor = rotation(PORT4);
+motor robotIntake = motor(PORT15 , blueCartridge, false);
+motor leftCata = motor(PORT3, redCartridge, false);
+motor rightCata = motor(PORT3, redCartridge, true);
+motor_group robotCata = motor_group(leftCata, rightCata);
+rotation cataSensor = rotation(PORT4);
 
-motor climbMotorA = motor(PORT8, redCartridge, true);
-motor climbMotorB = motor(PORT19, redCartridge, false);
-motor_group robotClimb = motor_group(climbMotorA, climbMotorB);
+digital_out leftWing = digital_out(robotBrain.ThreeWirePort.E);
+digital_out rightWing = digital_out(robotBrain.ThreeWirePort.H);
 
-digital_out leftPneumatic = digital_out(robotBrain.ThreeWirePort.E);
-digital_out rightPneumatic = digital_out(robotBrain.ThreeWirePort.H);
+// function definitions
+void resetCatapult(void);
+void increaseCounters(void);
 
-// controller input
-bool stopLeft = true;
-bool stopRight = true;
+int runDrivetrain(void);
+void toggleIntake(void);
+void toggleOuttake(void);
+void toggleCatapult(void);
+void toggleWings(void);
+void toggleLeftWing(void);
+void toggleRightWing(void);
 
-int controllerInput() {
-  while (true) {
+// auton definition
+void auton1(void);
+void auton2(void);
+void auton3(void);
+void auton4(void);
+
+// competition global
+competition Competition;
+
+// robot globals
+bool stopLeft = 1;
+bool stopRight = 1;
+bool outtakeTime = 0;
+
+// spin catapult with sensor
+void spinCatapultTo(int pos, directionType dir) {
+  while (cataSensor.position(degrees) != pos) {
+    robotCata.spin(dir, 100, rpm);
+  } robotCata.stop();
+}
+
+// robot setup
+void preAuton(void) {
+  task controllerInputTask(runDrivetrain);
+  resetCatapult();
+  
+  robotDrive.setDriveVelocity(100, percent);
+  robotDrive.setTurnVelocity(100, percent);
+  robotDrive.setStopping(hold);
+  robotIntake.setVelocity(100, percent);
+}
+
+// autonomous function
+void autonomous(void) { auton1(); }
+
+// driver control function
+void usercontrol(void) {
+  robotDrive.setDriveVelocity(100, percent);
+  robotDrive.setTurnVelocity(100, percent);
+  robotDrive.setStopping(brake);
+
+  while (1) {
+    increaseCounters();
+    resetCatapult();
+
+    robotController.ButtonL1.pressed(toggleCatapult);
+    robotController.ButtonR2.pressed(toggleIntake);
+    robotController.ButtonR2.released(toggleOuttake);
+    robotController.ButtonA.pressed(toggleWings);
+    robotController.ButtonL2.pressed(toggleLeftWing);
+    robotController.ButtonR2.pressed(toggleRightWing);
+
+    if (outtakeTime > 25) robotIntake.stop();
+
+    wait(20, msec);
+  }
+}
+
+// main function
+int main() {
+  Competition.autonomous(autonomous);
+  Competition.drivercontrol(usercontrol);
+
+  preAuton();
+
+  while (1) wait(100, msec);
+}
+
+// reset catapult
+void resetCatapult(void) {
+  double pos = robotCata.position(deg);// catapultSensor.position(deg);
+  
+  while (pos >= 360) pos -= 360;
+  while (pos <= -360) pos += 360;
+
+  if (robotCata.position(deg) != pos) robotCata.setPosition(pos, deg);
+  // if (catapultSensor.position(deg) != pos) catapultSensor.setPosition(pos, deg);
+}
+
+// increase counters
+void increaseCounters(void) {
+  outtakeTime ++;
+}
+
+// drivetrain function
+int runDrivetrain(void) {
+  while (1) {
     int speedLeft = robotController.Axis3.position() - robotController.Axis1.position();
     int speedRight = robotController.Axis3.position() + robotController.Axis1.position();
     
     if (speedLeft < 5 && speedLeft > -5) {
         if (stopLeft) {
             leftDrive.stop();
-            stopLeft = false;
+            stopLeft = 0;
         }
-    } else stopLeft = true;
+    } else stopLeft = 1;
 
     if (speedRight < 5 && speedRight > -5) {
         if (stopRight) {
             rightDrive.stop();
-            stopRight = false;
+            stopRight = 0;
         }
-    } else stopRight = true;
+    } else stopRight = 1;
     
     if (stopLeft) {
         leftDrive.setVelocity(speedLeft, percent);
@@ -68,128 +159,13 @@ int controllerInput() {
   } return 0;
 }
 
-// function definitions
-void runIntake(void);
-void toggleCatapult(void);
-void toggleWings(void);
-void toggleLeftWing(void);
-void toggleRightWing(void);
-void climbUp(void);
-void climbDown(void);
-
-void auton1(void);
-void auton2(void);
-void auton3(void);
-void auton4(void);
-
-// competition global
-competition Competition;
-
-// intake globals
-bool intaking = false;
-bool outtaking = false;
-bool climbing = false;
-
-// modulo function
-void resetSensor(void) {
-  double pos = robotCatapult.position(deg);// catapultSensor.position(deg);
-  
-  while (pos >= 360) pos -= 360;
-  while (pos <= -360) pos += 360;
-
-  if (robotCatapult.position(deg) != pos) robotCatapult.setPosition(pos, deg);
-  // if (catapultSensor.position(deg) != pos) catapultSensor.setPosition(pos, deg);
-}
-
-// spin catapult with sensor
-void spinCatapultTo(int pos, directionType dir) {
-  while (catapultSensor.position(degrees) != pos) {
-    robotCatapult.spin(dir, 100, rpm);
-  } robotCatapult.stop();
-}
-
-// setup robot
-void preAuton(void) {
-  // initialization
-  task controllerInputTask(controllerInput);
-  
-  // initial settings
-  robotIntake.setVelocity(600, rpm);
-  robotCatapult.setVelocity(100, rpm);
-  robotClimb.setStopping(hold);
-}
-
-// autonomous function
-void autonomous(void) {
-  // reset catapult
-  resetSensor();
-  
-  // drivetrain settings
-  robotDrive.setDriveVelocity(100, percent);
-  robotDrive.setStopping(hold);
-
-  // auton function
-  auton1();
-}
-
-// driver controlled function
-void usercontrol(void) {
-  // drivetrain settings
-  robotDrive.setDriveVelocity(100, percent);
-  robotDrive.setTurnVelocity(100, percent);
-  robotDrive.setStopping(brake);
-
-  while (1) {
-    // reset catapult encoders
-    resetSensor();
-
-    // robot functions
-    runIntake();
-    robotController.ButtonL1.pressed(toggleCatapult);
-    robotController.ButtonA.pressed(toggleWings);
-    robotController.ButtonLeft.pressed(toggleLeftWing);
-    robotController.ButtonRight.pressed(toggleRightWing);
-    robotController.ButtonUp.pressed(climbUp);
-    robotController.ButtonDown.pressed(climbDown);
-
-    robotBrain.Screen.print(robotClimb.position(degrees));
-
-    // waiting
-    wait(50, msec);
-    robotBrain.Screen.clearScreen();
-  }
-}
-
-// main function
-int main() {
-  // setting up callbacks
-  Competition.autonomous(autonomous);
-  Competition.drivercontrol(usercontrol);
-
-  // initializing robot
-  preAuton();
-
-  // prevent exiting the loop
-  while (true) wait(100, msec);
-}
-
 // intake function
-void runIntake(void) {
-  // detect controller input
-  bool intake = robotController.ButtonR1.pressing();
-  bool outtake = robotController.ButtonR2.pressing();
+void toggleIntake(void) { robotIntake.spin(forward); }
 
-  // intake / outtake toggle
-  if (intake && !outtake && !intaking) {
-    robotIntake.spin(forward);
-    intaking = 1; outtaking = 0;
-  } else if (outtake && !intake && !outtaking) {
-    robotIntake.spin(reverse);
-    intaking = 0; outtaking = 1;
-  } else {
-    robotIntake.stop();
-    intaking = 0; outtaking = 0;
-  }
+// outtake function
+void toggleOuttake(void) {
+  robotIntake.spin(reverse);
+  outtakeTime = 0;
 }
 
 // catapult function
@@ -199,16 +175,17 @@ void toggleCatapult(void) {
 
 // wings functions
 void toggleWings(void) {
-  leftPneumatic.set(!leftPneumatic.value());
-  rightPneumatic.set(!rightPneumatic.value());
+  if (leftWing.value() || rightWing.value()) {
+    leftWing.set(false);
+    rightWing.set(false);
+  } else {
+    leftWing.set(true);
+    rightWing.set(true);
+  }
 }
 
-void toggleLeftWing(void) { leftPneumatic.set(!leftPneumatic.value()); }
-void toggleRightWing(void) { rightPneumatic.set(!rightPneumatic.value()); }
-
-// climbing function
-void climbUp(void) { robotClimb.spinToPosition(robotClimb.position(degrees) - 90, degrees); }
-void climbDown(void) { robotClimb.spinToPosition(robotClimb.position(degrees) + 90, degrees); }
+void toggleLeftWing(void) { leftWing.set(!leftWing.value()); }
+void toggleRightWing(void) { rightWing.set(!rightWing.value()); }
 
 // auton on same team side
 void auton1(void) { }
